49e762106779fadc88a45914c5d2dcce792f0be9
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-03-24 12:16:38 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    // document.getElementById("input").addEventListener('click', function(){
+    //     alert("I got clicked");
+    // })
+
+    let input = document.getElementById("input");
+
+    input.addEventListener('mouseover', function() {
+        alert("The cursor got on me");
+    })
 }
